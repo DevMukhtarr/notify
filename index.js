4aaa69b5bacbfad31d07940e0cdf3cb1729da506
Mukhtar <mukhtarabdulfatai@gmail.com>
2023-12-08 21:51:17 +0100

    testing base

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const port = 4400 || process.env.PORT
 app.set('view engine', 'ejs'); 
 
 app.get("/", (req, res) =>{
-    res.render('index')
+    // res.render('index')
+    res.send("Hi")
 })
 
 app.listen(port, () =>{
