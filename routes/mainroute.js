6525e44ad8eb99eb87e61e6663026a43b159df8b
Mukhtar <mukhtarabdulfatai@gmail.com>
2023-12-09 18:29:46 +0100

    added dummy

--- routes/mainroute.js
+++ routes/mainroute.js
@@ -16,8 +16,8 @@ router.get("/profile", (req, res) =>{
 })
 
 router.post("/send-details", verifyToken,sendDetailsToAdmin)
-router.get("/alerts", verifyToken,viewAlerts, (req, res) =>{
-    res.render('/alerts')
+router.get("/alerts", (req, res) =>{
+    res.render('alerts')
 })
 
 export default router;
